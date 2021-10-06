a7d0ea54e63d9152e19739a8a90ac17516a4364e
Arthur Bitencourt <arthur.bitencourt@estudante.ufscar.br>
2021-10-05 22:06:35 -0300

    fix connection to mongodb

--- readCsv.py
+++ readCsv.py
@@ -5,7 +5,7 @@ spark = SparkSession.builder \
     .master("local") \
     .appName("readCsv") \
     .config("spark.mongodb.output.uri", "mongodb://127.0.0.1:27017/Projeto.netflix?authSource=admin") \
-    .config("spark.jars.package", "org.mongodb.spark:mongo-spark-connector_2.12-3.0.1,org.mongodb:mongo-java-driver-3.12.10") \
+    .config("spark.jars.package", "org.mongodb.spark:mongo-spark-connector_2.12-3.0.1,org.mongodb:mongo-java-driver:3.12.10") \
     .getOrCreate()
 
 df = spark.read.load("netflix/netflix_titles.csv", format="csv", sep=",", inferSchema="true", header="true")
@@ -14,9 +14,9 @@ df2 = df.select("title", "director")
 
 df2.show()
 
+df2.printSchema()
+
 df2.write \
-.format("com.mongodb.spark.sql.DefaultSource") \
+.format("mongo") \
 .mode("append") \
-.option("database", "Projeto") \
-.option("collection", "netflix") \
 .save()

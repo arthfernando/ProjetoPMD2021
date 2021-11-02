37b31aba883c21482ce8bd286a3198689c955627
Arthur Fernando <arthur.fernando03@gmail.com>
2021-11-02 19:20:52 -0300

    data formatada

--- readCsv.py
+++ readCsv.py
@@ -1,6 +1,15 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import when, col
+from pyspark.sql.functions import *
+
+
+def formatDate(x):
+    splitcol = split(x["date_added"], "-")
+    # mes = (splitcol.getItem(0))
+    mes = from_unixtime(unix_timestamp(splitcol.getItem(0), "MMMM"), "MM")
+    dia = splitcol.getItem(1)
+    ano = splitcol.getItem(2)
+    return (concat_ws("/", dia,mes,ano))
 
 # Cria sessao do spark
 spark = SparkSession.builder \
@@ -11,7 +20,6 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # le o arquivo csv
-# df = spark.read.load("netflix/netflix_titles2.csv", format="csv", sep=",", inferSchema="true", header="true")
 df = spark.read.options(header='True', inferSchema='True', delimiter=',', quote='\"', escape="\"").csv("netflix/netflix_titles.csv")
 
 # substitui o valor null para NAO_INFORMADO
@@ -34,20 +42,15 @@ df3 = df2.withColumn("rating", when(df2.rating == "G", "L") \
     .when(df2.rating == "TV-MA", "18") \
     .otherwise(df2.rating))
 
+# formata a data para MMMM-dd-yyyy
+df4 = df3.withColumn("date_added", regexp_replace(df3.date_added, "\s|(,\s)", "-"))
 
-# df3.select("rating").distinct().show()
+# formata a data para dd/MM/yyyy
+df5 = df4.withColumn("date_added", formatDate(df4))
 
-# df2 = df.createOrReplaceTempView("TABLE")
-# spark.sql("SELECT * FROM TABLE where rating = 'Benn Northover'").show()
-
-
-
-# df2.show()
-
-# df2.printSchema()
 
 # Salva o DataFrame no MongoDB
-df3.write \
+df5.write \
 .format("mongo") \
 .mode("append") \
 .save()
